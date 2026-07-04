2001512ae4164019b7c3f1068254a0f5582d63bd
falfia <22552895+falfiya@users.noreply.github.com>
2026-07-04 00:34:26 -0700

    Old SQL

--- client/src/db/migrations/00_init.sql
+++ client/src/db/migrations/00_init.sql
@@ -1,76 +1,81 @@
 -- Video data is fetched using the .snippet field of PlaylistItem.
 -- We keep track of very basic information.
-create table strings(
+create table _strings(
    id integer primary key,
    value text unique
 ) strict;
-insert into strings(id, value) values (0, null);
-insert into strings(id, value) values (1, '');
+insert into _strings(id, value) values (0, null);
+insert into _strings(id, value) values (1, '');
 
-create table videos(
-   id integer references strings(id) primary key
+--------------------------------------------------------------------------------
+-- Basic Entity Ids
+create table _channels(
+   id integer references _strings(id) primary key
 ) strict;
 
-create table channels(
-   id integer references strings(id) primary key
+create table _videos(
+   id integer references _strings(id) primary key
 ) strict;
 
-create table playlists(
-   id integer references strings(id) primary key
+create table _playlists(
+   id integer references _strings(id) primary key
 ) strict;
 
-create table playlist_items(
+create table _playlist_items(
    -- PlaylistItem ids are globally unique and durable
-   id integer references strings(id),
+   id integer references _strings(id) primary key,
    -- and the video never changes for that item.
-   vid integer references videos(id),
+   vid integer references _videos(id),
    -- Nor does the playlist it belongs to
-   pid integer references playlists(id)
+   pid integer references _playlists(id)
 ) strict;
 
+--------------------------------------------------------------------------------
 -- The actual data of most objects doesn't change much over time and is
 -- deduplicated into these tables and addressed using a single integer.
-create table video_data(
+create table _channel_data(
    id integer primary key,
-   owner integer references channels(id),
-   title integer references strings(id)
+   title integer references _strings(id) unique
 ) strict;
 
-create table playlist_data(
+create table _video_data(
    id integer primary key,
-   title integer references strings(id),
-   desc integer references strings(id)
+   owner integer references _channels(id),
+   title integer references _strings(id),
+   unique(owner, title)
 ) strict;
 
-create table video_at(
-   epoch integer not null,
-   vid integer references videos(id),
-   data integer references video_data(id)
+create table _playlist_data(
+   id integer primary key,
+   title integer references _strings(id),
+   desc integer references _strings(id),
+   unique(title, desc)
 ) strict;
-
-create table channel_data(
-   id integer not null,
-   title integer references strings(id)
-) strict;
-
+--------------------------------------------------------------------------------
 -- xxx_at tables track the change of data over time.
 -- It is expected that each snapshot, many rows are created in each of these.
 -- Since most data will be the same, a single integer is a lot cheaper than
 -- storing a bunch of integers.
-create table channel_at(
+create table _channel_at(
    epoch integer not null,
-   cid integer references channels(id),
-   data integer references channel_data(id)
+   cid integer references _channels(id),
+   data integer references _channel_data(id)
 ) strict;
 
-create table playlist_at(
+create table _video_at(
    epoch integer not null,
-   pid integer references playlists(id),
-   data integer references playlist_data(id)
+   vid integer references _videos(id),
+   data integer references _video_data(id)
 ) strict;
 
-create table playlist_item_at(
+create table _playlist_at(
    epoch integer not null,
-   pid integer references playlist_items(id),
+   pid integer references _playlists(id),
+   data integer references _playlist_data(id)
+) strict;
+
+create table _playlist_item_at(
+   epoch integer not null,
+   pid integer references _playlist_items(id),
    position integer not null
 ) strict;
